ae5bb2d1d899cf6895faa124dc8265940ff510d4
Rafael Leal Guimarães <rafaellealg18@gmail.com>
2025-03-13 23:07:38 -0300

    #5: Adicionado controllers e telas iniciais

--- agenda/src/main/java/com/agenda/agenda/controllers/CadastroController.java
+++ agenda/src/main/java/com/agenda/agenda/controllers/CadastroController.java
@@ -1,20 +1,16 @@
 package com.agenda.agenda.controllers;
-    
+
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-
 
 @Controller
-public class MainController {
-	
-	@GetMapping("/")
+public class CadastroController {
+    @GetMapping("/cadastro")
 	//@RequestMapping({"/","","/*"})
 	public String principal(Model m) {
-		return "index";
+		return "cadastro/cadastro";
 		
 	}
-
 }
--- agenda/src/main/java/com/agenda/agenda/controllers/LoginController.java
+++ agenda/src/main/java/com/agenda/agenda/controllers/LoginController.java
@@ -1,20 +1,15 @@
 package com.agenda.agenda.controllers;
-    
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-
 
 @Controller
-public class MainController {
-	
-	@GetMapping("/")
+public class LoginController {
+    @GetMapping("/login")
 	//@RequestMapping({"/","","/*"})
 	public String principal(Model m) {
-		return "index";
+		return "login/login";
 		
 	}
-
 }
